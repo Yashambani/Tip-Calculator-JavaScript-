1bc8a3855d400d68a52cb424f2d5fd99cc628b27
Yash Ambani <yambaniy@gmail.com>
2021-07-07 12:54:25 -0400

    var replaced with let

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,8 +1,9 @@
 var btn = document.querySelector("#calButton");
+
 btn.addEventListener("click", () => {
-  var billAmount = document.querySelector("#billAmount").value;
-  var tipOptions = document.querySelectorAll("#tip option");
-  var tipPerc, totalPeople, totalTipAmountEach;
+  let billAmount = document.querySelector("#billAmount").value;
+  let tipOptions = document.querySelectorAll("#tip option");
+  let tipPerc, totalPeople, totalTipAmountEach;
   tipOptions.forEach((item) => {
     if (item.selected) {
       tipPerc = item.value;
